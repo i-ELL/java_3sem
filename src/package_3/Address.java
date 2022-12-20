ccc61c78da0eaad00364cd7f9f9bedf856ebff92
Ell <87142229+i-ELL@users.noreply.github.com>
2022-12-20 22:12:15 +0300

    Add files via upload

---
+++ src/package_3/Address.java
@@ -0,0 +1,26 @@
+package package_3;
+
+
+
+public class Address {
+    String country;
+    String region;
+    String city;
+    String street;
+    String houseNum;
+    String building;
+    String flat;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "country='" + country + '\'' +
+                ", region='" + region + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNum='" + houseNum + '\'' +
+                ", building='" + building + '\'' +
+                ", flat='" + flat + '\'' +
+                '}';
+    }
+}
