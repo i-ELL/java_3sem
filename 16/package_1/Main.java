207ba27960669175d5171ea9762371285de7bc36
Ell <87142229+i-ELL@users.noreply.github.com>
2022-12-22 22:21:37 +0300

    Add files via upload

---
+++ 16/package_1/Main.java
@@ -0,0 +1,7 @@
+package package_1;
+
+public class Main {
+    public static void main(String[] args) {
+        new Game();
+    }
+}
---
+++ 16/package_3/Main.java
@@ -0,0 +1,7 @@
+package package_3;
+
+public class Main {
+    public static void main(String[] args) {
+        new TextAppearanceEditor();
+    }
+}
